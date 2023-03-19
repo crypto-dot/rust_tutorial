f6df98df7db4be2bbf1e3ea9341482af8071df04
Carlos Arbizu <carlos2345532@gmail.com>
2023-03-19 00:28:26 -0500

    Practicing with different variable types in rust

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rust_tutorial
 Becoming crab 🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀
+
+##Purpose 
+Learning the ins and outs of rust.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#![allow(unused)]
+use std::io;
+use rand::Rng;
+use std::io::{Write, ErrorKind,  BufReader, BufRead};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let mut name = String::new();   
+    let greeting = "Hello";
+    print!("Input your name: "); 
+    io::stdout().flush();
+    // print typically requires a flush afterwards since it is buffered to memory
+    io::stdin().read_line(&mut name).expect("Didn't receive input");
+    println!("Hello, {}! {}", name.trim(), greeting);
+    variables();
+}
+// Learning the different types of variables 
+fn variables() {
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.1415926;
+    let age: &str = "47";
+    let mut age: u32 = age.trim().parse().expect("Age wasn't assigned a number");
+    println!("I'm {} and I want ${}", age, ONE_MIL);
 }
